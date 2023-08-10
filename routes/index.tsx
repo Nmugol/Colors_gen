bd9fd5167014e0f7baf54c7e07347aebe272e496
Norbert Muzolf <nmugol000@gmail.com>
2023-08-10 16:43:49 +0200

    fix mising file

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,5 @@
 import { Head } from "$fresh/runtime.ts";
 import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
 
 import Header from "../components/Header.tsx"
 import Card from "../components/Card.tsx";
